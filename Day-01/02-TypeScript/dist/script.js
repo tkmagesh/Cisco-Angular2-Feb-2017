5eb9d3835dfbc9db9010256d7824fce88f488a18
Magesh <tkmagesh77@gmail.com>
2017-02-20 15:20:40 +0530

    TypeScript complete

--- Day-01/02-TypeScript/dist/script.js
+++ Day-01/02-TypeScript/dist/script.js
@@ -1,17 +1,57 @@
-function add(x, y) {
-    return x + y;
-}
-//add("100", "abc");
-var Employee = (function () {
-    //id : number = 0;
-    function Employee(id) {
-        this.id = id;
-        //this.id = id;
+/*class SalaryCalculator{
+    public basic : number = 0;
+    public hra : number = 0;
+    public da : number = 0;
+    public tax : number = 0;
+    public salary : number = 0;
+
+    calculate(){
+        var gross = this.basic + this.hra + this.da;
+        var net = gross * ((100-this.tax)/100);
+        this.salary = net;
     }
-    Employee.prototype.display = function () {
-        console.log(this.id);
+}*/
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Events = (function () {
+    function Events() {
+        this._eventListeners = {};
+    }
+    Events.prototype.onChange = function (attrName, fn) {
+        this._eventListeners[attrName] = this._eventListeners[attrName] || [];
+        this._eventListeners[attrName].push(fn);
     };
-    return Employee;
+    Events.prototype.trigger = function (attrName) {
+        var eventListeners = this._eventListeners[attrName] || [];
+        for (var i = 0; i < eventListeners.length; i++)
+            eventListeners[i]();
+    };
+    return Events;
 }());
-var emp = new Employee(100);
-console.log(emp.id);
+;
+var SalaryCalculator = (function (_super) {
+    __extends(SalaryCalculator, _super);
+    function SalaryCalculator() {
+        _super.apply(this, arguments);
+        this._data = {
+            basic: 0,
+            hra: 0,
+            da: 0,
+            tax: 0,
+            salary: 0
+        };
+    }
+    SalaryCalculator.prototype.get = function (attrName) {
+        return this._data[attrName];
+    };
+    SalaryCalculator.prototype.set = function (attrName, value) {
+        if (this._data[attrName] === value)
+            return;
+        this._data[attrName] = value;
+        this.trigger(attrName);
+    };
+    return SalaryCalculator;
+}(Events));
--- Day-01/02-TypeScript/src/script.ts
+++ Day-01/02-TypeScript/src/script.ts
@@ -1,19 +1,54 @@
-function add(x:number, y:number):number{
-	return x + y;
+/*class SalaryCalculator{
+	public basic : number = 0;
+	public hra : number = 0;
+	public da : number = 0;
+	public tax : number = 0;
+	public salary : number = 0;
+
+	calculate(){
+		var gross = this.basic + this.hra + this.da;
+		var net = gross * ((100-this.tax)/100);
+		this.salary = net;
+	}
+}*/
+
+class Events{
+	_eventListeners = {};
+
+	onChange(attrName : string, fn : Function) : void{
+		this._eventListeners[attrName] = this._eventListeners[attrName] || [];
+		this._eventListeners[attrName].push(fn);
+	}
+	trigger(attrName : string){
+		var eventListeners = this._eventListeners[attrName] || [];
+		for(var i=0; i < eventListeners.length; i++)
+			eventListeners[i]();
+	}
 }
 
-//add("100", "abc");
+interface ICalculatorData{
+	basic : number,
+	hra : number, 
+	da : number,
+	tax : number,
+	salary : number
+};
 
-class Employee{
-	//id : number = 0;
+class SalaryCalculator extends Events{
+	_data : ICalculatorData = {
+		basic : 0,
+		hra : 0,
+		da : 0,
+		tax : 0,
+		salary : 0
+	};
 
-	constructor(private id:number){
-		//this.id = id;
+	get(attrName : string) : any {
+		return this._data[attrName];
 	}
-	display(){
-		console.log(this.id);
+	set(attrName : string, value : any) : void{
+		if (this._data[attrName] === value) return;
+		this._data[attrName] = value;
+		this.trigger(attrName);
 	}
 }
-
-var emp : Employee = new Employee(100);
-console.log(emp.id);
