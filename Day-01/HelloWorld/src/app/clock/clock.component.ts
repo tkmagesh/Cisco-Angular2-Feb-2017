afe8c4211e7e60f2f4e1cb4c35037129b9f1e255
Magesh <tkmagesh77@gmail.com>
2017-02-21 11:38:15 +0530

    Handling events and accessing text box values

--- Day-01/HelloWorld/src/app/clock/clock.component.ts
+++ Day-01/HelloWorld/src/app/clock/clock.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 @Component({
 	selector : 'clock',
-	template : '<div><small><i>{{currentTime}}</i></small></div>'
+	template : `
+	<div>
+		<small>
+			<i>{{currentTime}}</i>
+		</small>
+		<input type="button" value="Refresh" (click)="updateTime()">
+	</div>
+	`
 })
 export class ClockComponent{
 	currentTime :string = Date();
+
 	constructor(){
-		setInterval( () => this.currentTime = Date(), 1000);
+		//setInterval( () => this.currentTime = Date(), 1000);
+	}
+
+	updateTime(){
+		this.currentTime = Date();
 	}
 }
--- Day-01/HelloWorld/src/app/hello-world/helloWorld.component.ts
+++ Day-01/HelloWorld/src/app/hello-world/helloWorld.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector : 'hello-world',
-	template : '<h2>{{message}}</h2>'
+	template : `
+	<label>Name :</label>
+	<input type="text" #txtName>
+	<input type="button" value="Greet" (click)="onGreetClick(txtName.value)">
+	<h2>{{message}}</h2>
+	`
 })
 export class HelloWorldComponent{
-	message : string = 'Welcome to the world of Angular2!'
+	message : string = '';
+
+	onGreetClick(name){
+		
+		this.message = 'Hi ' + name + ', Welcome to the world of Angular2!';
+	}
 }
